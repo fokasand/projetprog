66864f351ea2498efed843b9371c55b5ab6c1c43
fokasand <sandovalkaterina@gmail.com>
2016-03-15 13:34:34 +0100

    Update Collider.hpp

--- partie1/src/Env/Collider.hpp
+++ partie1/src/Env/Collider.hpp
@@ -1,12 +1,33 @@
 
+#include <cassert>
 #ifndef COLLIDER_H
 #define COLLIDER_H
+#include <../Utility/Vec2d.hpp>
 
 class Collider 
 {
-	
-	
-	
+	private :
+			Vec2d centre;
+			double rayon;
+	public :
+	//constructeur, verifier que r positif ou nul,
+			Collider (Vec2d c, double r) :
+			centre(c),
+			rayon(r)
+			{
+				assert(rayon >=0);
+			}
+			
+	//getters
+			const Vec2d& position ()
+			{
+				return centre;
+			}
+			
+			double ()
+			{
+				return rayon;
+			}
 }
 
 
