fe8433076048373cc87e0dfb7236894a18ddd25d
lubacien <lucien.barret@gmail.com>
2016-03-18 09:28:10 +0100

    Update Collider.hpp

--- partie1/src/Env/Collider.hpp
+++ partie1/src/Env/Collider.hpp
@@ -4,6 +4,8 @@
 #define COLLIDER_H
 #include "Utility/Vec2d.hpp"
 
+using namespace std;
+
 class Collider 
 {
 	private :
@@ -12,16 +14,20 @@ class Collider
 	public :
 	
 	//constructeur, verifier que r positif ou nul,
-			Collider (Vec2d c, double r);
-			Collider (const Collider& a);
+	Collider (Vec2d c, double r);
+	Collider (const Collider& a);
 			
 	//getters
 	const Vec2d& getPosition() const;
 	double getRadius() const;
 			
 	//déplacement
+	Vec2d directionTo (Collider c) const;
+	
+	// distances
 	Vec2d directionTo (Vec2d to) const;
-	Vec2d Collider::directionTo (Collider c) const
+	double distanceTo(Vec2d to) const;
+	
 	//clamping
 	void clamping(Vec2d& c);
 	
@@ -34,9 +40,16 @@ class Collider
 	bool isColliding(const Collider& other) const;
 	bool isPointInside(const Vec2d& point) const;
 	
-	// distances
-	Vec2d directionTo (Vec2d to) const;
-	double distanceTo(Vec2d to) const;
+	//opérateurs
+	bool operator>(const Vec2d& point);
+	bool operator>(const Collider& body2);
+	bool operator|(const Collider& body2);
+	
+	//aide pour <<
+	
+	std::ostream& affiche(std::ostream& sortie) const;
+	
+	
 	
 			
 };
