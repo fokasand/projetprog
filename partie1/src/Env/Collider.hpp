db6b101ab1862c20fd35a371f84ae8ab3035b7da
lubacien <lucien.barret@gmail.com>
2016-03-19 16:33:29 +0100

    Update Collider.hpp

--- partie1/src/Env/Collider.hpp
+++ partie1/src/Env/Collider.hpp
@@ -14,7 +14,7 @@ class Collider
 	public :
 	
 	//constructeur, verifier que r positif ou nul,
-	Collider (Vec2d c, double r);
+	Collider (const Vec2d& c,const double& r);
 	Collider (const Collider& a);
 			
 	//getters
@@ -22,14 +22,14 @@ class Collider
 	double getRadius() const;
 			
 	//déplacement
-	Vec2d directionTo (Collider c) const;
-	Vec2d directionTo (Vec2d to) const
-	void  move(const Vec2d& dx);
+	Vec2d directionTo (const Collider& c) const;
+	Vec2d directionTo (Vec2d to) const;
+	void  move(Vec2d dx);
 	
 	
 	// distances
-	;
-	double distanceTo(Vec2d to) const;
+	
+	double distanceTo(const Vec2d& to) const;
 	
 	//clamping
 	void clamping();
@@ -44,19 +44,20 @@ class Collider
 	bool isPointInside(const Vec2d& point) const;
 	
 	//opérateurs
-	bool operator>(const Vec2d& point);
-	bool operator>(const Collider& body2);
-	bool operator|(const Collider& body2);
+	
+	bool operator>(const Vec2d& point) const;
+	bool operator>(const Collider& body2) const;
+	bool operator|(const Collider& body2) const;
 	Collider& operator+=(const Collider& b); // I4m not sure if return type should be Collider or Collider&
 	
 	//aide pour <<
 	
 	std::ostream& affiche(std::ostream& sortie) const;
-	
-	
-	
-			
+
 };
+
+std::ostream& operator<<(std::ostream& sortie, const Collider& c);
+
 
 
 #endif
