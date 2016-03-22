b9e024b13bba7becccbc73a2a91e025e1ef49a23
fokasand <sandovalkaterina@gmail.com>
2016-03-22 14:19:00 +0100

    Update World.hpp

--- World.hpp
+++ World.hpp
@@ -3,21 +3,22 @@
 
 class World{
 	private :
-	//variable regroupant la texture totale à afficher
-			 sf::RenderTexture renderingCache_;
-	//sommets liés à chaque texture
+			//variable regroupant la texture totale à afficher
+			sf::RenderTexture renderingCache_;
+			
+			//sommets liés à chaque texture
 			 vector <sf::Vertex> grassVertexes_, 
-				   		waterVertexes_,
-						rockVertexes_;
+								waterVertexes_,
+								 rockVertexes_;
 	public:
-	
+			
+			//initialisation ensembles de textures
+			void reloadCacheTructure();
+
+			//draw
+			void drawOn(sf::RenderTarget& target);
 	};
 
-//initialisation ensembles de textures
-void reloadCacheTructure();
-
-//draw
-void drawOn(sf::RenderTarget& target);
 
 #endif
 
