cadc400b59ffa3b9aaaf141192d61baff1a2d60a
fokasand <sandovalkaterina@gmail.com>
2016-03-22 14:18:26 +0100

    Update World.cpp

--- World.cpp
+++ World.cpp
@@ -3,6 +3,21 @@
 #include <World.hpp>
 #include <iostream>
 
+//initialisation ensemble de textures
+void World::reloadCacheTructure()
+
+{	//vector contenant l'ensemble de sommets
+	vector<sf::Vertex> Vertexes (generateVertexes(getTerrain["textures"],nbCell ,cellSize));
+	
+	//initialisation à tous les sommets de la grille
+	grassVertexes_=Vertexes;
+	waterVertexes_=Vertexes;
+	rockVertexes_=Vertexes;
+	
+	//initialisation de la texture
+	renderingCache_.create(nbCell*cellSize, nbCell*cellSize);
+}
+
 void World::drawOn(sf::RenderTarget& target)
 {
 	sf::Sprite cache(renderingCache_.getTexture());
