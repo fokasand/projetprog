1c7857fadb21e01d11c9f9ae3a11d8eeced1fc93
fokasand <sandovalkaterina@gmail.com>
2016-04-05 12:21:09 +0200

    Update World.hpp

--- World.hpp
+++ World.hpp
@@ -1,7 +1,13 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+
 enum class Kind : short { grass, water, rock};
+struct Seed {
+	sf::Vector2i coord;
+	Kind nature;	
+};
+
 class World{
 	private :
 			//variable regroupant la texture totale à afficher
@@ -11,10 +17,19 @@ class World{
 			 std::vector <sf::Vertex> grassVertexes_, 
 								waterVertexes_,
 								 rockVertexes_;
-								 
+			
+			//tableau de cellules					 
 			std::vector<Kind> cells_;
 			int nbCells_;
 			float cellSize_;
+			
+			//graines
+			std::vector<Seed> seeds_;		
+			int nbWater_;
+			int nbGrass_;
+			
+			//coloriage et transparence
+			void colour (std::string tex, Kind type, std::vector <sf::Vertex> vertex);
 	public:
 			//mettre a jour rendering_Cache
 			void updateCache();
@@ -31,11 +46,13 @@ class World{
 			//reset
 			void reset(bool regenerate=true);
 			
+			//fichier
 			void loadFromFile();
+			
+			
 	};
 	
 j::Value getTerrain();
 
 
 #endif
-
