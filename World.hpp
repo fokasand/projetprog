7818412e9291e6e6fa8e4fee1959981747d4cfdd
fokasand <sandovalkaterina@gmail.com>
2016-04-05 19:47:01 +0200

    Update World.hpp

--- World.hpp
+++ World.hpp
@@ -30,9 +30,23 @@ class World{
 			
 			//colore ou met en transparence les sommets dans chache vertex et 
 			void colour (std::string tex, Kind type, std::vector <sf::Vertex> vertex);
+			
+			//conversions
+			int toUnid (int x, int y); // to unidimensional 
+			sf::Vector2i toBid( int x); // to bidimensional
+			
+			//débordements de la fenetre d'affichage
+			
+			//cordonnée superieure a nbCells
+			int debSup (int c);
+			//coordonnée inferieur à nbCells
+			int debInf(int c);
+			//verifie le vecteur de coordonnées
+			void debVect (sf::Vector2i coord); 
+			
 	public:
 			
-			//charge les paramètres  de la fenetre à part
+			//charge les paramètres  de la fenetre à partir du fichier
 			void reloadConfig();
 			
 			//nettoie renderingCache, y dessine les 3 Vertexes en couleur ou transparence selon le type de la cellule, affiche la cache
@@ -55,13 +69,11 @@ class World{
 			
 			void steps( int i, bool regeneration);
 			
-			//conversions
-			int toUnid (int x, int y); // to unidimensional 
-			sf::Vector2i toBid( int x); // to bidimensional
 			
-	};
+		};
 	
 j::Value getTerrain();
 
 
 #endif
+
