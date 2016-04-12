c4b6fc1eb9e74179dd3afce38792fffabd63059e
fokasand <sandovalkaterina@gmail.com>
2016-04-12 13:03:01 +0200

    Update Environnement.hpp

--- Environnement.hpp
+++ Environnement.hpp
@@ -1,1 +1,16 @@
 
+#ifndef ENVIRONNEMENT_H
+#define ENVIRONNEMENT_H
+
+class Environnement {
+	
+	//regeneration de l'environnement
+	void reset();
+	//evolution de l'environnement sur un temps dt
+	void update(sf::Time dt);
+	
+	//dessin de l'environnement
+	void drawOn(sf::RenderTarget& target);
+};
+
+#endif
