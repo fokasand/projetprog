201e3e6c7abbf5f0e84ac058700fc07245cc8af8
fokasand <sandovalkaterina@gmail.com>
2016-04-12 13:04:22 +0200

    Update Environnement.cpp

--- Environnement.cpp
+++ Environnement.cpp
@@ -1,1 +1,23 @@
 
+#include <Application.hpp>
+#include "World.hpp"
+#include <SFML/Graphics.hpp>
+#include "Environnement.hpp"
+
+	//regeneration de l'environnement
+	void Environnement::reset()
+	{
+		World::reset();
+	}
+	
+	//evolution de l'environnement sur un temps dt
+	void Environnement::update(sf::Time dt)
+	{
+		World::updateCache();
+	}
+	
+	//dessin de l'environnement
+	void Environnement::drawOn(sf::RenderTarget& target)
+	{
+		World::drawOn();
+	}
