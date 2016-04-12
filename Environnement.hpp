c060c2e8c8d98a6e557b6ddd3a7f87b1bf3d9a32
fokasand <sandovalkaterina@gmail.com>
2016-04-12 22:28:31 +0200

    Update Environnement.hpp

--- Environnement.hpp
+++ Environnement.hpp
@@ -1,17 +1,31 @@
-#ifndef ENVIRONNEMENT_H
-#define ENVIRONNEMENT_H
+#ifndef ENV_H
+#define ENV_H
+#include "World.hpp"
 
-class Environnement {
+class Env {
 	
-	private:
-	World World_;
+	public:
+	//constucteur
+	Env();
+	
+	//attribus
+	World world_;
+	
 	//regeneration de l'environnement
 	void reset();
+	
 	//evolution de l'environnement sur un temps dt
 	void update(sf::Time dt);
 	
 	//dessin de l'environnement
 	void drawOn(sf::RenderTarget& target);
+	
+	//génération de l'environnement à partir d'un fichier
+	void loadWorldFromFile();
+	
+	//sauvegarder l'environnement dans un ficher
+	void saveWorldToFile();	
+	
 };
 
 #endif
