9d3f1e379231fed0a605a1e20c4c1bb9db7ca6be
fokasand <sandovalkaterina@gmail.com>
2016-04-19 21:19:09 +0200

    Update Environnement.cpp

--- Environnement.cpp
+++ Environnement.cpp
@@ -23,6 +23,12 @@ Env::Env()
 void Env::reset()
 {
 	world_.reset(true);
+	for (size_t i(0); i < flowers_.size() ; ++i)
+	{
+		flowers_[i].reset();
+	}
+	flowers_.clear();
+	
 }
 
 //evolution de l'environnement sur un temps dt
@@ -34,22 +40,44 @@ void Env::update(sf::Time dt)
 //dessin de l'environnement
 void Env::drawOn(sf::RenderTarget& target)
 {
+	//dessin du terrain
 	world_.drawOn(target);
+	
+	//dessin de toutes les fleurs une par une
+	for (size_t i(0); i < flowers_.size() ; ++i)
+	{
+		flowers_[i]->drawOn(target); 
+	}
+	
 }
 
 // ajout de fleur dans env
-void Env::addFlowerAt (Vec2d p)
+bool Env::addFlowerAt (Vec2d p)
 {
 	//la cellule correspondant à p doit être de l'herbe
-	if (world_.World::isGrowable(p) and flowers_.size()< getEnv()["max flowers"].toInt())
+	unsigned int maxFlower (getEnv()["max flowers"].toInt());
+	if (world_.isGrowable(p) and flowers_.size()< maxFlower )
 	{
 		double min (getEnv()["initial"]["flower"]["nectar"]["min"].toDouble());
 		double max (getEnv()["initial"]["flower"]["nectar"]["max"].toDouble());
 		double r (getEnv()["initial"]["flower"]["size"]["manual"].toDouble());
-		flowers_.push_back(std::unique_ptr<Flower>  (new Flower(p,r,uniform(min,max))));
+		flowers_.push_back(std::unique_ptr<Flower> (new Flower(p,r,uniform(min,max))));	
+		
+		return true;
 	}
-
+	return false;
 }
+
+//afficher anneau
+void Env::drawFlowerZone(sf::RenderTarget& target,Vec2d const& position)
+{
+	sf::Color color = (world_.isGrowable(position)) ? sf::Color::Green : sf::Color::Red;
+	double size (getEnv()["initial"]["flower"]["size"]["manual"].toDouble());
+	auto shape = buildAnnulus(position, size, color, 0.5);
+	target.draw(shape);
+	
+}
+
 //génération d'un environnement à partir d'un fichier
 void Env::loadWorldFromFile()
 {
