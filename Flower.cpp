5f423e78905a801183311713e7024a09875ce167
fokasand <sandovalkaterina@gmail.com>
2016-04-19 14:08:36 +0200

    Update Flower.cpp

--- Flower.cpp
+++ Flower.cpp
@@ -1,11 +1,11 @@
 #include "Flower.hpp"
-#include <Application.hpp>
-#include <Random/Random.hpp>
 
-//constructeur de flower avec 3 paramètres
+
+//constructeur de flower avec 3 paramètres, initialise le collider, 
+//quantité de pollen et texture de la fleur à l'aide de l'indice max
 Flower::Flower (const Vec2d& c, const double& r, double qpollen) 
-: Collider::Collider(c, r), pollen(qpollen), 
-texture(getAppTexture(["simulation"]["flower"]["textures"][uniform(0,["simulation"]["flower"]["textures"].size()-1)].toString()))
+: Collider::Collider(c, r), pollen(qpollen), max(getFlower()["textures"].size()-1),
+texture(getAppTexture(getFlower()["textures"][uniform(0,max)].toString()))
 {}
 
 //retrait de pollen tant qu'il en reste 
@@ -16,8 +16,8 @@ double Flower::takePollen (double take)
 		pollen-= take; // on rend la quantité prélevée
 					   // si superieure à celle de pollen
 	}else {	
-		take = 0 ;		// sinon on rend 0 (n'affecte pas take car 
-						// le passage ne se fait pas par ref)
+		take= pollen; //on rend la quantité disponible
+		pollen-= take;
 	}
 	return take;
 }
@@ -29,4 +29,10 @@ void Flower::drawOn(sf::RenderTarget& target) const
 	auto flowerSprite = buildSprite(centre, rayon, texture);
       target.draw(flowerSprite);
 	
+}
+
+//racourci pour les données de configuration
+j::Value getFlower()
+{
+	return getAppConfig()["simulation"]["flower"];
 }
