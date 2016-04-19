2f014481c89870ed441dbd06391365e4f367a09d
fokasand <sandovalkaterina@gmail.com>
2016-04-19 14:07:16 +0200

    Update Environnement.cpp

--- Environnement.cpp
+++ Environnement.cpp
@@ -1,18 +1,20 @@
-#include <Application.hpp>
-#include <SFML/Graphics.hpp>
 #include "Env.hpp"
+
+//racourci données de configuration pour env
+j::Value getEnv()
+{
+	return getAppConfig()["simulation"]["env"];
+}
 
 //constructeur par défaut
 Env::Env()
 {
 	try {
-		std::cerr << "loadWorldFromFile" << std::endl;
 		loadWorldFromFile();
-		
 		}
 	catch(std::runtime_error error)
 	{
-		std::cerr << "regeneration aléatoire" << std::endl;
+		std::cerr << "fichier non ouvert : regeneration aléatoire" << std::endl;
 		world_.reset(true);
 	}
 }
@@ -20,14 +22,13 @@ Env::Env()
 //regeneration de l'environnement
 void Env::reset()
 {
-	std::cerr << "env reset" << std::endl;
 	world_.reset(true);
 }
 
 //evolution de l'environnement sur un temps dt
 void Env::update(sf::Time dt)
 {
-	
+
 }
 
 //dessin de l'environnement
@@ -36,12 +37,28 @@ void Env::drawOn(sf::RenderTarget& target)
 	world_.drawOn(target);
 }
 
+// ajout de fleur dans env
+void Env::addFlowerAt (Vec2d p)
+{
+	//la cellule correspondant à p doit être de l'herbe
+	if (world_.World::isGrowable(p) and flowers_.size()< getEnv()["max flowers"].toInt())
+	{
+		double min (getEnv()["initial"]["flower"]["nectar"]["min"].toDouble());
+		double max (getEnv()["initial"]["flower"]["nectar"]["max"].toDouble());
+		double r (getEnv()["initial"]["flower"]["size"]["manual"].toDouble());
+		std::unique_ptr<Flower> p1 (new Flower(p,r,uniform(min,max)));
+		flowers_.push_back(p1);
+		
+	}
+
+}
 //génération d'un environnement à partir d'un fichier
 void Env::loadWorldFromFile()
 {
 	world_.loadFromFile();
 }
 
+// sauvegarde 
 void Env::saveWorldToFile()
 {
 	world_.saveToFile();
