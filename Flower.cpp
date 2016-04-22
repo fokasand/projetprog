b280299acbf2f1a841992f36ab18e9ae8ed38cf3
fokasand <sandovalkaterina@gmail.com>
2016-04-22 18:15:18 +0200

    Update Flower.cpp

--- Flower.cpp
+++ Flower.cpp
@@ -1,13 +1,11 @@
 #include "Flower.hpp"
 
-
 //constructeur de flower avec 3 paramètres, initialise le collider, 
 //quantité de pollen et texture de la fleur à l'aide de l'indice max
 Flower::Flower (const Vec2d& c, const double& r, double qpollen) 
 : Collider::Collider(c, r), pollen(qpollen), max(getFlower()["textures"].size()-1),
 texture(getAppTexture(getFlower()["textures"][uniform(0,max)].toString()))
 {}
-
 //retrait de pollen tant qu'il en reste 
 double Flower::takePollen (double take)
 {
