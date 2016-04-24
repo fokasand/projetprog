cc0f854182bfe12727c2cd0c5260527477d0dc24
fokasand <sandovalkaterina@gmail.com>
2016-04-24 18:43:35 +0200

    Added files via upload

---
+++ FlowerGen.cpp
@@ -0,0 +1,11 @@
+#include "FlowerGen.hpp"
+
+void FlowerGen::update(sf::Time dt)
+{
+	
+}
+
+void FlowerGen::reset()
+{
+	t=sf::Time::Zero;
+}
---
+++ FlowerGen.hpp
@@ -0,0 +1,17 @@
+#ifndef FLOWERGEN_H
+#define FLOWERGEN_H
+#include <SFML/Graphics.hpp>
+
+class FlowerGenerator
+{
+public :
+	
+	void update(sf::Time dt);
+	void reset();
+	
+private :
+
+	sf::Time t;
+
+};
+#endif
