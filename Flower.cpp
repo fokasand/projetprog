40c7478ddb355f36f27f511102fcc5959c3a06d8
lubacien <lucien.barret@gmail.com>
2016-04-24 22:01:17 +0200

    Update Flower.cpp

--- Flower.cpp
+++ Flower.cpp
@@ -1,4 +1,6 @@
 #include "Flower.hpp"
+#include "Env.hpp"
+
 
 //constructeur de flower avec 3 paramètres, initialise le collider, 
 //quantité de pollen et texture de la fleur à l'aide de l'indice max
@@ -6,6 +8,7 @@ Flower::Flower (const Vec2d& c, const double& r, double qpollen)
 : Collider::Collider(c, r), pollen(qpollen), max(getFlower()["textures"].size()-1),
 texture(getAppTexture(getFlower()["textures"][uniform(0,max)].toString()))
 {}
+
 //retrait de pollen tant qu'il en reste 
 double Flower::takePollen (double take)
 {
@@ -23,8 +26,10 @@ double Flower::takePollen (double take)
 //dessiner les fleurs
 void Flower::drawOn(sf::RenderTarget& target) const 
 {
+	
 	auto flowerSprite = buildSprite(centre, rayon, texture);
       target.draw(flowerSprite);
+	
 }
 
 //racourci pour les données de configuration
