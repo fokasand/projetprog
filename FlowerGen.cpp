c49922ed6df0bdad88039a9a8c30406e2004b62c
lubacien <lucien.barret@gmail.com>
2016-04-29 21:44:41 +0200

    Update FlowerGen.cpp

--- FlowerGen.cpp
+++ FlowerGen.cpp
@@ -1,14 +1,17 @@
 #include "FlowerGen.hpp"
 #include "Env.hpp"
+#include <Random/Random.hpp>
+#include <Application.hpp>
+
 
 void FlowerGenerator::update(sf::Time dt)
 {
     t+=dt;
-    if ((t >= sf::seconds(getAppConfig()["simulation"]["flower generator"]["delay"].toDouble())) and
+    if ((t >= sf::seconds(getAppConfig()["simulation"]["flower generator"]["delay"].toDouble())) and //vérifie que le temps est écoulé, et que la génération aléatoire est activée
         (getAppConfig()["simulation"]["flower generator"]["active"].toBool())) {
-        reset();
+        reset(); // remet le compteur à zéro
         Vec2d p(uniform(0.0, getApp().getWorldSize().x), uniform(0.0, getApp().getWorldSize().y));
-        getAppEnv().addFlowerAt(p); // mais si ca marche pas parce que c'est du rocher? osef?
+        getAppEnv().addFlowerAt(p);
     }
 }
 
