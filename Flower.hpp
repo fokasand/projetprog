5ba02a949acf139cf0441b2081cf1b7dd00387aa
lubacien <lucien.barret@gmail.com>
2016-04-29 13:28:19 +0200

    Update Flower.hpp

--- Flower.hpp
+++ Flower.hpp
@@ -10,30 +10,31 @@
 
 j::Value getFlower(); // raccourci pour acceder aux données
 
-class Flower : public Collider, public Drawable, public Updatable {
-	
-	public :
-	//constructeur de Flower avec 3 paramètres
-	Flower (const Vec2d& c, const double& r, double qpollen);
-	
-	//retirer du pollen tant qu'il est disponible
-	double takePollen (double take);
-	
-	double getPollen(); // utilisé dans Env pour tuer les fleurs.
-	
-	//dessiner les fleurs
-	virtual void drawOn(sf::RenderTarget& target) const override;
-	
-	//mettre a jour la fleur (quantité de pollen)
-	virtual void update(sf::Time dt) override;
-	
-	protected : 
-	
-	double pollen;
-	// indice maximal de texture -> creation d'un int pour pouvoir utiliser uniform
-	int max; 
-	//initialisation de la textre de la fleur, ne change plus
-	sf::Texture const texture;
+class Flower : public Collider, public Drawable, public Updatable
+{
+
+public :
+    //constructeur de Flower avec 3 paramètres
+    Flower (const Vec2d& c, const double& r, double qpollen);
+
+    //retirer du pollen tant qu'il est disponible
+    double takePollen (double take);
+
+    double getPollen() const; // utilisé dans Env pour tuer les fleurs.
+
+    //dessiner les fleurs
+    virtual void drawOn(sf::RenderTarget& target) const override;
+
+    //mettre a jour la fleur (quantité de pollen)
+    virtual void update(sf::Time dt) override;
+
+protected :
+
+    double pollen;
+    // indice maximal de texture -> creation d'un int pour pouvoir utiliser uniform
+    int max;
+    //initialisation de la textre de la fleur, ne change plus
+    sf::Texture const texture;
 
 };
 
