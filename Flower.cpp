3024cf1a51e12dc2415e42826bb730d758761001
lubacien <lucien.barret@gmail.com>
2016-04-29 21:43:35 +0200

    Update Flower.cpp

--- Flower.cpp
+++ Flower.cpp
@@ -1,5 +1,7 @@
 #include "Flower.hpp"
 #include "Env.hpp"
+#include <Random/Random.hpp>
+#include <Application.hpp>
 
 
 //constructeur de flower avec 3 paramètres, initialise le collider,
@@ -7,21 +9,23 @@
 Flower::Flower (const Vec2d& c, const double& r, double qpollen)
     : Collider::Collider(c, r), pollen(qpollen), max(getFlower()["textures"].size()-1),
       texture(getAppTexture(getFlower()["textures"][uniform(0,max)].toString()))
-{
-    (*this).clamping(); // ne suffit pas. pourquoi?
-}
+{} // la fleur est clampée automatiquement
 
 //retrait de pollen tant qu'il en reste
 double Flower::takePollen (double take)
 {
-    if ( pollen >= take) {
-        pollen-= take; // on rend la quantité prélevée
-        // si superieure à celle de pollen
-    } else {
-        take= pollen; //on rend la quantité disponible
-        pollen-= take;
-    }
-    return take;
+	if (pollen > 0 )
+	{
+		if ( pollen >= take) {
+			pollen-= take; // on rend la quantité prélevée
+			// si superieure à celle de pollen
+		} else {
+			take= pollen; //on rend la quantité disponible
+			pollen-= take;
+		}
+		return take;
+	}
+	return 0;
 }
 
 //dessiner les fleurs
@@ -43,21 +47,23 @@ void Flower::update(sf::Time dt)
 {
     if (pollen<= 0) 
     {
-		cout << " bonjour" << endl;
-        getAppEnv().killFlower(); // dit qu'une fleur est morte, pour que Env regarde laquelle est morte et l'enlève du tableau.
+        getAppEnv().killFlower(); // dit qu'une fleur est morte, pour que une méthode dans Env regarde laquelle est morte et l'enlève du tableau.
     } else 
     {
-        pollen += dt.asSeconds() * log( getAppEnv().world_.humid(centre) / getFlower()["growth"]["threshold"].toDouble() );
-        if (pollen >= getFlower()["growth"]["split"].toDouble()) {
+        pollen += dt.asSeconds() * log( getAppEnv().howhumid(centre) / getFlower()["growth"]["threshold"].toDouble() );
+        if (pollen >= getFlower()["growth"]["split"].toDouble()) 
+        {
             Vec2d pp;
             int essaismax(0);
             do {
                 double d (uniform(0.5*rayon, 2*rayon));
                 pp = centre + Vec2d::fromRandomAngle() * d;
                 ++essaismax;
-            } while ( (!getAppEnv().addFlowerAt(pp)) and (essaismax <100) ); // dans add flower at, rajouter un test que 2 fleurs ne se collident pas.
-
+            } while ( (!getAppEnv().addFlowerAt(pp)) and (essaismax <=100) );
+			if (essaismax != 100) // la quantité de pollen n'est divisée par 2 que si on a bien fait la fleur.
+			{
             pollen = pollen/2;
+			}
         }
     }
 }
