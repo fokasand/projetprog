55122d5a58917c6c9274d4eea89216dfcfbf87be
fokasand <sandovalkaterina@gmail.com>
2016-05-08 22:00:54 +0200

    Update Bee.hpp

--- Bee.hpp
+++ Bee.hpp
@@ -1,10 +1,48 @@
-#ifndef BEE_H
-#define BEE_H
+#ifndef HIVE_H
+#define HIVE_H
+#include "Collider.hpp"
+#include <Application.hpp>
+#include <Interface/Updatable.hpp>
+#include <Interface/Drawable.hpp>
 
-class Bee{
+class Bee;
+class Hive : public Collider, public Drawable, public Updatable{
+public:
 	
+	//constructeur
+	Hive() = default;
+	Hive(const Vec2d& c);
 	
+	//bloquer copie
+	Hive(Hive const& other) = delete;
+	void operator =(Hive const& other) = delete;
+	
+	//destructeur
+	~Hive();
+	
+	//ajouter una abeille à la ruche
+	void addBee();
+	
+	//faire évoluer les abeilles
+	void update(sf::Time dt);
+	
+	void drawOn(sf::RenderTarget& targetWindow) const override;
+	
+	//ajouter du nectar à la ruche
+	void dropPollen(double qte);
+	
+	//retirer du nectar de la ruche
+	void takeNectar(double qte);
+	
+private:
+	double nectar_;
+	vector <Bee*> bees_;
+	sf::Texture const texture;
 	
 };
 
-#endif 
+//racourci pour les données de configuration
+j::Value getHive();
+
+#endif
+
