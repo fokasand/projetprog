6f947e73082e1e92e885b87dcf7206195a43cf13
lubacien <lucien.barret@gmail.com>
2016-05-09 19:24:31 +0200

    Update Collider.hpp

--- partie1/src/Env/Collider.hpp
+++ partie1/src/Env/Collider.hpp
@@ -6,51 +6,48 @@
 
 using namespace std;
 
-class Collider 
+class Collider
 {
-	private :
-	
-	Vec2d centre;
-	double rayon;
-	
-	public :
-	
-	friend void swap(Collider& a, Collider& b);
-	
-	//constructeur, verifier que r positif ou nul,
-	Collider (const Vec2d& c,const double& r);
-	Collider (const Collider& a);
-			
-	//getters
-	const Vec2d& getPosition() const;
-	double getRadius() const;
-			
-	//déplacement
-	Vec2d directionTo (Collider c) const;
-	Vec2d directionTo (Vec2d to) const;
-	void move(const Vec2d& dx);
-	
-	// distances
-	
-	double distanceTo(Vec2d to) const;
-	double distanceTo(Collider c) const;
-	
-	//clamping
-	void clamping();
+protected :
+    Vec2d centre;
+    double rayon;
+public :
 
-	// booléens
-	
-	bool isColliderInside(const Collider& other) const;
-	bool isColliding(const Collider& other) const;
-	bool isPointInside(const Vec2d& point) const;
-	
-	// =
-	
-	Collider& operator=(Collider b);
-	
-	//aide pour <<
-	
-	std::ostream& affiche(std::ostream& sortie) const;
+    friend void swap(Collider& a, Collider& b);
+
+    //constructeur, verifier que r positif ou nul,
+    Collider (const Vec2d& c,const double& r);
+    Collider (const Collider& a);
+
+    //getters
+    const Vec2d& getPosition() const;
+    double getRadius() const;
+
+    //déplacement
+    Vec2d directionTo (Collider c) const;
+    Vec2d directionTo (Vec2d to) const;
+    void move(const Vec2d& dx);
+
+    // distances
+
+    double distanceTo(Vec2d to) const;
+    double distanceTo(Collider c) const;
+
+    //clamping
+    void clamping();
+
+    // booléens
+
+    bool isColliderInside(const Collider& other) const;
+    bool isColliding(const Collider& other) const;
+    bool isPointInside(const Vec2d& point) const;
+
+    // =
+    Collider& operator=(Collider b);
+
+    //aide pour <<
+
+    std::ostream& affiche(std::ostream& sortie) const;
 
 };
 
@@ -59,10 +56,11 @@ std::ostream& operator<<(std::ostream& sortie, const Collider& c);
 bool operator>(const Collider& body, const Vec2d& point);
 bool operator>(const Collider& body1,const Collider& body2);
 bool operator|(const Collider& body1,const Collider& body2);
-void operator+=(Collider& c,const Vec2d& b); 
+void operator+=(Collider& c,const Vec2d& b);
 
 //aide pour =
 void swap(Collider& a, Collider& b);
 
+void clamping_(Vec2d& pos);
 
 #endif
