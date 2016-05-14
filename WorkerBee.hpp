3212bd0d1f7d030be208d54be8d08e1b7156c11c
lubacien <lucien.barret@gmail.com>
2016-05-14 15:54:06 +0200

    Update WorkerBee.hpp

--- WorkerBee.hpp
+++ WorkerBee.hpp
@@ -1,23 +1,17 @@
 #ifndef WORKERBEE_H
 #define WORKERBEE_H
 #include "Bee.hpp"
+#include "Hive.hpp"
 
 class WorkerBee : public Bee 
 {
 public:
-	//constructeur de workerbee qui ne prend aucun paramètres, va seulement les chercher dans le fichier de configuration.
-	/*WorkerBee();
+	//constructeur
+	WorkerBee(Vec2d centre, Hive* hive);
 	//racourci pour les données de configuration
-	j::Value Bee::getWorkerConfig() const 
-	{
-		return getAppConfig()["simulation"]["bees"]["worker"];
-	}
+	j::Value const& getWorkerConfig() const;
 
-	j::Value Bee::getConfig() const
-	{
-		return getWorkerConfig();
-	}
-	*/ 
+	j::Value const& getConfig() const;
 private:
 
 	
