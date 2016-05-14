74d4807fc05f5e9c90dc7f42d2bb7d978b0ab8c5
lubacien <lucien.barret@gmail.com>
2016-05-14 15:53:42 +0200

    Update WorkerBee.cpp

--- WorkerBee.cpp
+++ WorkerBee.cpp
@@ -1,3 +1,19 @@
 #include "WorkerBee.hpp"
+#include <Application.hpp>
 
-//WorkerBee::WorkerBee() : Bee(
+WorkerBee::WorkerBee(Vec2d centre, Hive* hive) : Bee( centre, getWorkerConfig()["size"].toDouble(), hive, 
+													getWorkerConfig()["energy"]["initial"].toDouble(), 
+													getWorkerConfig()["speed"].toDouble())
+													{}
+
+//racourci pour les données de configuration
+j::Value const& WorkerBee::getWorkerConfig() const
+	{
+		return getAppConfig()["simulation"]["bees"]["worker"];
+	}
+
+j::Value const& WorkerBee::getConfig() const
+	{
+		return getWorkerConfig();
+	}
+
