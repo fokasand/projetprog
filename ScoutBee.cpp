c7686d7e75f022adf3cea1a94d3fbe9b3fa9a23d
lubacien <lucien.barret@gmail.com>
2016-05-14 15:52:51 +0200

    Update ScoutBee.cpp

--- ScoutBee.cpp
+++ ScoutBee.cpp
@@ -1,1 +1,21 @@
+#include "ScoutBee.hpp"
+#include <Application.hpp>
+
+ScoutBee::ScoutBee(Vec2d centre, Hive* hive) : Bee( centre, getScoutConfig()["size"].toDouble(), hive, 
+													getScoutConfig()["energy"]["initial"].toDouble(), 
+													getScoutConfig()["speed"].toDouble()) 
+													{}
+
+//racourci pour les données de configuration
+j::Value const& ScoutBee::getScoutConfig() const
+{
+	return getAppConfig()["simulation"]["bees"]["scout"];
+}
+
+j::Value const& ScoutBee::getConfig() const
+{
+	return getScoutConfig();
+}
+
+
 
