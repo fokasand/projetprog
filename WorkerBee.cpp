4f958acd5da4a32e0dd70f83f5e399ed4eff284b
fokasand <sandovalkaterina@gmail.com>
2016-05-16 16:20:09 +0200

    Update WorkerBee.cpp

--- WorkerBee.cpp
+++ WorkerBee.cpp
@@ -1,18 +1,27 @@
 #include "WorkerBee.hpp"
 #include <Application.hpp>
 
-State const WorkerBee::IN_HIVE = createUid();
 State const WorkerBee::GET_POLLEN = createUid();
 State const WorkerBee::RETURN_HIVE = createUid();
 
 //constructeur
-WorkerBee::WorkerBee(Vec2d centre, Hive* hive, vector<State> states) : 
+WorkerBee::WorkerBee(Vec2d centre, Hive* hive) : 
 Bee( centre, getWorkerConfig()["size"].toDouble(), hive, 
 	getWorkerConfig()["energy"]["initial"].toDouble(), 
-	getWorkerConfig()["speed"].toDouble()),
-CFSM(states)
-{}
+	getWorkerConfig()["speed"].toDouble(),
+	getAppTexture(getWorkerConfig()["texture"].toString()))
+{
+	setloss();
+	states_.push_back(GET_POLLEN);
+	states_.push_back(RETURN_HIVE);
+}
 
+//éxecution des actions liées à l'état courant
+void WorkerBee::onState(State current,sf::Time dt)
+{
+//TODO: à coder	
+}
+	
 //racourci pour les données de configuration
 j::Value const& WorkerBee::getWorkerConfig() const
 {
@@ -23,4 +32,3 @@ j::Value const& WorkerBee::getConfig() const
 {
 	return getWorkerConfig();
 }
-
