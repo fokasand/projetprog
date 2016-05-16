663b5fe1c0da22922d02e600b9ab69a5eff179f2
lubacien <lucien.barret@gmail.com>
2016-05-16 10:36:03 +0200

    Update ScoutBee.cpp

--- ScoutBee.cpp
+++ ScoutBee.cpp
@@ -1,11 +1,12 @@
 #include "ScoutBee.hpp"
 #include <Application.hpp>
 
-ScoutBee::ScoutBee(Vec2d centre, Hive* hive) : 
-Bee( centre, getScoutConfig()["size"].toDouble(), hive, 
-	getScoutConfig()["energy"]["initial"].toDouble(), 
-	getScoutConfig()["speed"].toDouble()) 
-{}
+ScoutBee::ScoutBee(Vec2d centre, Hive* hive) : Bee( centre, getScoutConfig()["size"].toDouble(), hive, 
+													getScoutConfig()["energy"]["initial"].toDouble(), 
+													getScoutConfig()["speed"].toDouble(),
+													getAppTexture(getScoutConfig()["texture"].toString()))
+													
+													{}
 
 //racourci pour les données de configuration
 j::Value const& ScoutBee::getScoutConfig() const
@@ -17,6 +18,5 @@ j::Value const& ScoutBee::getConfig() const
 {
 	return getScoutConfig();
 }
-
 
 
