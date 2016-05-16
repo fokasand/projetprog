b9239c3943c11b7171410ec8d2f1a47ae3093949
fokasand <sandovalkaterina@gmail.com>
2016-05-16 16:41:44 +0200

    Update ScoutBee.hpp

--- ScoutBee.hpp
+++ ScoutBee.hpp
@@ -8,12 +8,17 @@ class ScoutBee : public Bee
 public:
 	//constructeur
 	ScoutBee(Vec2d centre, Hive* hive);
+	
+	//éxecution des actions liées à l'état courant
+	void onState(State current,sf::Time dt) override;
+	
 	//racourci pour les données de configuration
 	j::Value const& getScoutConfig() const;
 
 	j::Value const& getConfig() const;
-private:
 	
+private:
+	static const State IN_HIVE;	
 };
 
 #endif
