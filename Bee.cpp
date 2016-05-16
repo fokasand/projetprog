4ea28cdd2d834ef3706d2ec2b33826221b2cfa71
fokasand <sandovalkaterina@gmail.com>
2016-05-16 16:18:24 +0200

    Update Bee.cpp

--- Bee.cpp
+++ Bee.cpp
@@ -3,20 +3,25 @@
 #include <Application.hpp>
 #include <Random/Random.hpp>
 #include "Env.hpp"
+
+State const Bee::IN_HIVE = createUid();
+vector<State> Bee::etats_ =  vector<State> (IN_HIVE);
+
 //constructeur
 Bee::Bee(Vec2d centre,
-			double rayon,
-			 Hive* hive,
-			 double energy, 
-			 double amplitude,
-			 sf::Texture texture)
-:Collider(centre,rayon), 
+			double rayon,Hive* hive,
+			double energy,
+			double amplitude,
+			sf::Texture texture)
+: Collider(centre,rayon),
+CFSM (etats_),
 hive_(hive),
 energy_(energy),
 speed_(amplitude*Vec2d::fromRandomAngle()),
 texture_ (texture = getAppTexture(getBeeConfig()["texture"].toString())),
 prob(getBeeConfig()["moving behaviour"]["random"]["rotation probability"].toDouble()),
-alpha_max(getBeeConfig()["moving behaviour"]["random"]["rotation angle max"].toDouble())
+alpha_max(getBeeConfig()["moving behaviour"]["random"]["rotation angle max"].toDouble()),
+moveMode_(Rest)
 {}
 
 //morte si le niveau d'energie est nul
@@ -33,11 +38,12 @@ bool Bee::isDead()
 void Bee::update(sf::Time dt)
 {
 	//action liée à l'etat courant
-	action();// ou OnState
-
-	//baisse de l'énergie
-	energy_-=0.1*dt.asSeconds();
+	action(dt);// ou OnState
+	// movement et perte d'energie
+	move(dt);
 }
+
+//déplacement aléatoire
 void Bee::randomMove(sf::Time dt)
 {
 	//changement aléatoire de direction
@@ -76,9 +82,14 @@ void Bee::randomMove(sf::Time dt)
 	}
 }
 
+//déplacement ciblé
+void Bee::targetMove(sf::Time dt)
+{
+	//TODO: à coder
+}
+
 void Bee::drawOn(sf::RenderTarget& target) const 
 {
-
 	auto beeSprite = buildSprite(centre, rayon, texture_);
 	
 	if (( speed_.angle() >= M_PI/2) or (speed_.angle() <= -M_PI/2))
@@ -88,13 +99,58 @@ void Bee::drawOn(sf::RenderTarget& target) const
 	beeSprite.rotate(speed_.angle()/DEG_TO_RAD);
 	
     target.draw(beeSprite);
+    
+    if(isDebugOn())
+    {
+		//couleur et épaisseur dependent de l'état de mouvement de l'abeille
+		sf::Color color = (getMoveMode()==Random) ? sf::Color::Black : sf::Color::Blue; 
+		double size (rayon+1);
+		double thickness = (getMoveMode()==Random) ? 5 : 3;
+		auto shape = buildAnnulus(centre, size, color, thickness);
+		target.draw(shape);
+	}
+    
 }
 
 //déplacement non aléatoire
-void move(sf::Time dt)
+void Bee::move(sf::Time dt)
 {
+	switch(moveMode_)
+	{
+		case Rest:
+		{
+			energy_-=restloss_;
+			break;
+		}
+		
+		case Random:
+		{
+			energy_-=moveloss_;
+			randomMove(dt);
+			break;
+		}
+		
+		case Targeted:
+		{
+			targetMove(dt);
+			break;
+		}	
+	}
 	
 }
+
+//retourne le mode de déplacement 
+MoveMode Bee::getMoveMode() const
+{
+	return moveMode_;
+}
+
+void Bee::setloss()
+{
+	restloss_=getConfig()["energy"]["consumption rates"]["idle"].toDouble();
+	moveloss_=getConfig()["energy"]["consumption rates"]["moving"].toDouble();
+}
+
 
 j::Value const& Bee::getBeeConfig() const 
 {
