ecbafce38838ffb77006cfcc3c468fcf15fa7a57
fokasand <sandovalkaterina@gmail.com>
2016-05-16 10:47:24 +0200

    Update WorkerBee.cpp

--- WorkerBee.cpp
+++ WorkerBee.cpp
@@ -1,21 +1,26 @@
 #include "WorkerBee.hpp"
 #include <Application.hpp>
 
-WorkerBee::WorkerBee(Vec2d centre, Hive* hive) : Bee( centre, getWorkerConfig()["size"].toDouble(), 
-							hive, 
-							getWorkerConfig()["energy"]["initial"].toDouble(), 
-							getWorkerConfig()["speed"].toDouble(),
-							getAppTexture(getWorkerConfig()["texture"].toString()))
-							{}
+State const WorkerBee::IN_HIVE = createUid();
+State const WorkerBee::GET_POLLEN = createUid();
+State const WorkerBee::RETURN_HIVE = createUid();
+
+//constructeur
+WorkerBee::WorkerBee(Vec2d centre, Hive* hive, vector<State> states) : 
+Bee( centre, getWorkerConfig()["size"].toDouble(), hive, 
+	getWorkerConfig()["energy"]["initial"].toDouble(), 
+	getWorkerConfig()["speed"].toDouble()),
+CFSM(states)
+{}
 
 //racourci pour les données de configuration
 j::Value const& WorkerBee::getWorkerConfig() const
-	{
-		return getAppConfig()["simulation"]["bees"]["worker"];
-	}
+{
+	return getAppConfig()["simulation"]["bees"]["worker"];
+}
 
 j::Value const& WorkerBee::getConfig() const
-	{
-		return getWorkerConfig();
-	}
+{
+	return getWorkerConfig();
+}
 
