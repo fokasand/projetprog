f9b31b6e12a2d68d0b195c433fa70db21ad75e6e
fokasand <sandovalkaterina@gmail.com>
2016-05-16 10:45:13 +0200

    Update ScoutBee.cpp

--- ScoutBee.cpp
+++ ScoutBee.cpp
@@ -1,13 +1,20 @@
 #include "ScoutBee.hpp"
 #include <Application.hpp>
 
-ScoutBee::ScoutBee(Vec2d centre, Hive* hive) : Bee( centre, getScoutConfig()["size"].toDouble(), 
-						hive, 
-						getScoutConfig()["energy"]["initial"].toDouble(), 
-						getScoutConfig()["speed"].toDouble(),
-						getAppTexture(getScoutConfig()["texture"].toString()))
-						
-						{}
+State const WorkerBee::IN_HIVE = createUid()
+
+ScoutBee::ScoutBee(Vec2d centre, Hive* hive,vector<State> states) : 
+Bee( centre, getScoutConfig()["size"].toDouble(), hive, 
+	getScoutConfig()["energy"]["initial"].toDouble(), 
+	getScoutConfig()["speed"].toDouble()),
+CFSM(states)
+{}
+
+//éxecution des actions liées à l'état courant
+void onState(State current,sf::Time dt)
+{
+//TODO: à coder	
+}
 
 //racourci pour les données de configuration
 j::Value const& ScoutBee::getScoutConfig() const
