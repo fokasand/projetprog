a3ca4615508061d10aa2b068afe6e88564327189
fokasand <sandovalkaterina@gmail.com>
2016-05-26 18:55:36 +0200

    Update ScoutBee.hpp

--- ScoutBee.hpp
+++ ScoutBee.hpp
@@ -1,7 +1,6 @@
 #ifndef SCOUTBEE_H
 #define SCOUTBEE_H
 #include "Bee.hpp"
-#include "Hive.hpp"
 
 class ScoutBee : public Bee
 {
@@ -14,9 +13,13 @@ public:
 	void onEnterState(State state) override;
 	
 	//racourci pour les données de configuration
-	
-
 	virtual j::Value const& getConfig() const override;
+	
+	//méthodes permettant l'intéraction entre deux abeilles
+	virtual void interact(Bee* other) override;
+	virtual void interactWith(ScoutBee* scouting) override;
+	virtual void interactWith(WorkerBee* working) override;
+
 	
 private:
 	
@@ -25,9 +28,19 @@ private:
 	static const State TO_HIVE;
 	static std::vector<State> etats_ ;
 	
+	//energie minimale pour chercher une fleur
 	double const enmin_flower;
 	
+	//seuil pour la partage de la mémoire
+	double const max_share;
+	
+	//compteur pour le nombre de partages
+	int counter;
+	
+	//accès au fichier de configuration
 	static j::Value const& getScoutConfig();
+	
+	virtual void drawOn(sf::RenderTarget& targetWindow) const override;
 	
 };
 
