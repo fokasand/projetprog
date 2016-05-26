bdde9d985335c7b744608c6fe4a7e92dde67f4f3
fokasand <sandovalkaterina@gmail.com>
2016-05-26 18:56:41 +0200

    Update WorkerBee.cpp

--- WorkerBee.cpp
+++ WorkerBee.cpp
@@ -1,90 +1,58 @@
 #include "WorkerBee.hpp"
+#include "ScoutBee.hpp"
 #include <Application.hpp>
-#include "Env.hpp"
 
 State const WorkerBee::IN_HIVE = createUid();
-State const WorkerBee::TO_HIVE = createUid();
-State const WorkerBee::GET_POLLEN = createUid();
 State const WorkerBee::SEARCH_FLOWER = createUid();
-vector<State> WorkerBee::etats_ = {IN_HIVE, SEARCH_FLOWER, GET_POLLEN, TO_HIVE};
+State const WorkerBee::TO_HIVE = createUid();
+vector<State> WorkerBee::etats_ = vector<State> (IN_HIVE,TO_HIVE);
 
 //constructeur
 WorkerBee::WorkerBee(Vec2d centre, Hive* hive) : 
-Bee(etats_, // pour sncm
-	centre, getWorkerConfig()["size"].toDouble(), hive, // pour collider
+Bee( etats_,centre, getWorkerConfig()["size"].toDouble(), hive, 
 	getWorkerConfig()["energy"]["initial"].toDouble(), 
 	getWorkerConfig()["speed"].toDouble(),
-	getAppTexture(getWorkerConfig()["texture"].toString())),
-	
-	max_pollen_(getWorkerConfig()["max pollen capacity"].toDouble()),// attributs propres à la classe
-	harvestStep_(getWorkerConfig()["harvest rate"].toDouble()),
-	nectarStep_ (getWorkerConfig()["transfer rate"].toDouble())
+	getAppTexture(getWorkerConfig()["texture"].toString()))
 {
 	setTout();
-	states_.push_back(GET_POLLEN);
 }
 
-void WorkerBee::onState(State current, sf::Time dt)
+//éxecution des actions liées à l'état courant
+void WorkerBee::onState(State current,sf::Time dt)
 {
-	if(current==IN_HIVE)
-	{
-		if (nectarbee_ > 0)
-		{
-			nectarbee_ -= nectarStep_;
-			hive_->dropPollen(nectarStep_);
-			statestring_ = "in_hive_pollen";
-		}
-		else
-		{
-			if (energy_ < enmin_hive)
-			{
-				eat(dt);
-				statestring_ = "in_hive_eat";
-			}
-			else
-			{
-				if ( memory_ != nullptr)
-				{
-					if (energy_ > enmin_hive)
-					{
-						nextState();
-					}
-					statestring_="in_hive_???";
-				}
-				statestring_ = "in_hive_no_flower";
-			}
-		}
-	}
+//TODO: à coder	
+}
+
+void WorkerBee::onEnterState(State state)
+{
+	//TODO
+}
 	
-	if(current==SEARCH_FLOWER)
-	{
-		if (isPointInside(*memory_))
-		{
-			nextState();
-		}
-	}
+//racourci pour les données de configuration
+j::Value const& getWorkerConfig()
+{
+	return getAppConfig()["simulation"]["bees"]["worker"];
+}
+
+j::Value const& WorkerBee::getConfig() const
+{
+	return getWorkerConfig();
+}
+
+//methodes d'interaction
+void WorkerBee::interact(Bee* other)
+{
+	other->interactWith(this);
+}
+
+void WorkerBee::interactWith(ScoutBee* scouting)
+{
 	
-	if (current == GET_POLLEN)
-	{
-		if ( (getAppEnv().getCollidingFlower(*this) != nullptr) and (nectarbee_ < max_pollen_))
-		{
-		nectarbee_ += harvestStep_; // ajoute le pollen au stock de l'abeille
-		getAppEnv().getCollidingFlower(*this)->takePollen(harvestStep_); // retire le pollen de la fleur
-		statestring_ = "collecting_pollen";
-		}
-		else 
-		{
-			nextState();
-		}
-	}
-	if(current==TO_HIVE)
-	{
-		statestring_="back_to_hive";
-		if ((getAppEnv().getCollidingHive(*this)->getPosition() == target_))
-		{
-			nextState();
-		}
-	}
+}
+
+void WorkerBee::interactWith(WorkerBee* working)
+{
+	//nothing todo
 }
 
 void WorkerBee::drawOn(sf::RenderTarget& target) const 
@@ -98,57 +66,3 @@ void WorkerBee::drawOn(sf::RenderTarget& target) const
 	}
     
 }
-
-//éxecution des actions liées à l'état courant
-void WorkerBee::onEnterState(State current)
-{
-	if(current==IN_HIVE)
-	{
-		//effacer la mémoire pour qu'elle puisse être remplie par un scout dans la ruche.
-		memory_=nullptr;
-		// ajoute l'abeille à la waiting list
-		//hive_.addToList(this);
-		if (hive_->getNectar() > 0)
-		{
-			//méthode givenectar
-		}
-		
-		moveMode_ = Rest;
-	}
-	
-	if(current==SEARCH_FLOWER)
-	{
-		statestring_ = "to_flower";
-		target_ = *memory_;
-		moveMode_=Targeted;
-	}
-	
-	if(current==TO_HIVE)
-	{
-		//prendre la ruche comme cible de déplacment
-		target_=hive_->getPosition();
-		moveMode_=Targeted;
-	}
-}
-
-
-void WorkerBee::learnFlowerLocation(Vec2d flowerPosition)
-{
-	memory_ = &flowerPosition;
-}
-	
-//racourci pour les données de configuration
-j::Value const& WorkerBee::getWorkerConfig() const
-{
-	return getAppConfig()["simulation"]["bees"]["worker"];
-}
-
-j::Value const& WorkerBee::getConfig() const
-{
-	return getWorkerConfig();
-}
-
-
-
-
-
