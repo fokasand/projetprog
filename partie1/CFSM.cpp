fbd3f9aa9fff5f077a6f57825e9cb38ae73ed17c
fokasand <sandovalkaterina@gmail.com>
2016-05-29 10:33:10 +0200

    Update CFSM.cpp

--- partie1/CFSM.cpp
+++ partie1/CFSM.cpp
@@ -1,12 +1,13 @@
 #include "CFSM.hpp"
 #include <Application.hpp>
-//constructeur
+//constructeur : prend en paramètre un tableau d'etats
 CFSM::CFSM(std::vector<State>& states)
 : states_(states), current_(0)
 {}
 
 CFSM::~CFSM()=default;
-//rend l'état courant
+
+//rend l'état courant sous forme d'indice dans le tableau d'états
 State CFSM::getState()
 {
 	return current_; //
@@ -15,7 +16,7 @@ State CFSM::getState()
 //passe à l'état suivant
 void CFSM::nextState()
 {
-	//passer à l'état suivant si fin de la liste pas encore attent
+	//passer à l'état suivant si fin de la liste pas encore attente
 	if(current_< states_.size()-1)
 	{
 		++current_;
@@ -24,11 +25,11 @@ void CFSM::nextState()
 		//recommence au début de la liste des états
 		current_=0;
 	}
+	//actions d'entrée au nouvel état
 	onEnterState(states_[current_]);
 }
 
 //actions à l'état courant
-
 void CFSM::action(sf::Time dt)
 {
 	onState(states_[current_], dt);
