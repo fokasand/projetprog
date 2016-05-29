f8b887c122448fe6c506f06da368da74cf85a058
fokasand <sandovalkaterina@gmail.com>
2016-05-29 10:30:14 +0200

    Update Bee.hpp

--- partie1/Bee.hpp
+++ partie1/Bee.hpp
@@ -6,13 +6,15 @@
 #include <Interface/Drawable.hpp>
 #include "CFSM.hpp"
 
+//predéfinition des classes pour eviter la dépendance circulaire
 class WorkerBee;
 class ScoutBee;
 
+//definit les valeurs possibles pour le type de mouvement: Repos, mouvement Aléatoire ou mouvement Ciblé
 enum MoveMode {Rest,Random,Targeted};
 
 class Bee: public Collider, public Drawable, public Updatable, 
-		public CFSM 
+	public CFSM 
 {
 public:	
 	//constructeur
@@ -25,7 +27,6 @@ public:
 		sf::Texture texture);
 		 
 	// destructeur
-	
 	virtual ~Bee();
 		 	 
 	//morte si energie nulle
@@ -34,17 +35,18 @@ public:
 	//déplacement : calcule nouvelles positions et vitesse
 	void update(sf::Time dt);
 	
+	//dessin des abeilles
 	void drawOn(sf::RenderTarget& targetWindow) const;
 	
 	//méthodes de mouvement
 	//aléatoire
 	void randomMove(sf::Time dt);
-	//non aléatoire
-	void move(sf::Time dt);
 	//ciblé
 	void targetMove(sf::Time dt, Vec2d target);
+	//méthode appelant randomMove ou targetMove selon le mode de mouvement courant
+	void move(sf::Time dt);
 	
-	//permet d'ulitiser le polymophisme de getConfig
+	//permet d'ulitiser le polymophisme de getConfig pour initialiser des attributs
 	void setTout();
 	
 	//retourne le mode de déplacement 
@@ -57,27 +59,32 @@ public:
 	// essaye de bouger l'abeille selon son vecteur vitesse, si c est impossible, fais tourner la direction.
 	bool movebee(sf::Time dt);
 	
-	//manger la nectar
+	//manger du nectar
 	void eat(sf::Time dt);
 	
 	//passer une adresse à la mémoire
 	void setMemory(Vec2d* address);
 	
-	//rend la position de la fleur visible
+	//rend la position de la première fleur visible
 	Vec2d* visibleFlower();
 	
+	//méthode d'interaction non définis pour une abeille générique
 	virtual void interact(Bee* other) = 0;
 	virtual void interactWith(ScoutBee* scouting) = 0;
 	virtual void interactWith(WorkerBee* working) = 0;
 	
+	//indique si l'abeille se trouve dans la ruche ou non 
 	bool isBeeInHive();
 	
+	//rend la position en mémoire de l'abeille
 	Vec2d* getMemory() const;
 	
 private:
 	void changeEnergy(double quant);
+	double delay;
 	
 protected:
+	//pointeur sur sa ruche
 	Hive* hive_;
 	double energy_;
 	Vec2d speed_;
@@ -94,18 +101,15 @@ protected:
 	double restloss_;
 	double moveloss_;
 	Vec2d target_;
-	
-	//voir si ne peuvent être mis dans Bee avec un setter polymorphique
 	double cons_rate;
 	double enmin_hive;
 	double visibility_;
 	
+	//permet d'afficher l'état dans lequel se trouve l'abeille
 	string statestring_;
 	
 	//permet de repertorier les abeilles dans une ruche
 	bool isInHive_;
-	
-private:
-	double delay;
+
 };	
 #endif
